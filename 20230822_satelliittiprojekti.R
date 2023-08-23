9895035f8215d4b455912df5cdcccd7ec8f72299
Akselivv <135106425+Akselivv@users.noreply.github.com>
2023-08-23 17:33:15 +0300

    Chow-epäjatkuvuusalgoritmi
    Lisätty epäjatkuvuuskohtia Chow-epäjatkuvuustestin avulla etsivä ja visualisoiva alogritmi

--- 20230822_satelliittiprojekti.R
+++ 20230822_satelliittiprojekti.R
@@ -3,11 +3,31 @@ library(tidyverse)
 library(magrittr)
 library(ggplot2)
 
+chowDiscontinuityAlgorithm <- function(data) {
+  
+  valuevec <- c()
+
+  for (i in 3:(nrow(data)-3)) {
+    testresult <- (strucchange::sctest(as.numeric(as.POSIXct(data$to)) ~ as.numeric(data$NDVI_index), type = "Chow", point = i))
+    valuevec <- c(valuevec, testresult$statistic)
+  }
+  
+  valuevec %<>% unname()
+
+}
+
 getwd()
-filename <- "data10.json"
+filename <- "data30.json"
 hakkuuaika <- as.POSIXct("2019-06-15")
 rajaaVuoteen <- TRUE
 jatkuvuusanalyysi <- TRUE
+pivot <- FALSE
+
+if (rajaaVuoteen) {
+  title <- "Hakkuun vaikutus normalisoituun kasvillisuusindeksiin: rajaus, vuosi"
+} else {
+  title <- "Hakkuun vaikutus normalisoituun kasvillisuusindeksiin, ei aikarajausta"
+}
 
 data <- fromJSON(file = filename)
 data <- data[[1]]
@@ -29,7 +49,9 @@ for (i in 1:nobs) {
 }
 
 print(newdf)
-
+newdf$mean %<>% as.numeric()
+newdf$mean[is.infinite(newdf$mean)] <- NA
+newdf %<>% na.omit()
 
 if (rajaaVuoteen) {
   
@@ -39,8 +61,12 @@ if (rajaaVuoteen) {
   
 } 
 
-newdf <- newdf %>%
-  pivot_longer(!to, names_to = "critical_value", values_to = "NDVI_index")
+if (pivot) {
+  newdf <- newdf %>%
+    pivot_longer(!to, names_to = "critical_value", values_to = "NDVI_index")
+} else if (!pivot) {
+  colnames(newdf)[4] <- "NDVI_index"
+}
 
 newdf$hakkuu <- numeric(nrow(newdf))
 
@@ -56,6 +82,25 @@ if (!jatkuvuusanalyysi) {
     ggplot(aes(x = as.POSIXct(to), y = as.numeric(NDVI_index), 
                group=hakkuu, color=hakkuu)) +
     geom_point() +
-    geom_smooth(method="lm") 
+    geom_smooth(method="lm") +
+    xlab("Päivämäärä") + 
+    ylab("NDVI-indeksi") + 
+    ggtitle(title)
   
 }
+
+Chowvec <- chowDiscontinuityAlgorithm(newdf)
+
+ggplot(data.frame(Chow_test = Chowvec, index = c(1:length(Chowvec))), aes(x = index, y = Chow_test)) + geom_point()
+
+#timedf <- data.frame(to = as.POSIXct(seq.POSIXt(as.POSIXct("2020-11-27"), as.POSIXct("2022-08-10"), by = "1 week")),
+#                     NDVI_index = (sin(0.15*(1:length(seq.POSIXt(as.POSIXct("2020-11-27"), as.POSIXct("2022-08-10"), by = "1 week"))))))
+
+#Chowvec <- chowDiscontinuityAlgorithm(timedf)
+
+#ggplot(data.frame(Chow_test = Chowvec, index = c(1:length(Chowvec))), aes(x = index, y = Chow_test)) + geom_point()
+#ggplot(timedf, aes(y = NDVI_index, x = to)) + geom_point()
+
+diff(Chowvec)
+plot(diff(Chowvec))
+
